24b5518f94f8653f2483a2610b305d2d61c0dd89
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2016-11-20 22:54:10 +0200

    Requirement 3

--- src/models/clicker.ts
+++ src/models/clicker.ts
@@ -8,11 +8,21 @@ export class Clicker {
   private id: string;
   private name: string;
   private clicks: Array<Click>;
+  private icon: string;
 
-  constructor(id: string, name: string) {
+  constructor(id: string, name: string, availableIcons: Array<string> = [
+      'globe',
+      'grid',
+      'images',
+      'power',
+      'locked',
+      'mail',
+      'moon',
+      'paper-plane']) {
     this.id = id;
     this.name = name;
     this.clicks = [];
+    this.icon = availableIcons[this.getRandomInt(0, availableIcons.length - 1)];
   }
 
   public doClick(): void {
@@ -33,5 +43,17 @@ export class Clicker {
 
   public getName(): string {
     return this.name;
+  }
+
+  public getIcon(): string {
+    return this.icon;
+  }
+
+  /**
+   * Returns a random integer between min (inclusive) and max (inclusive)
+   * Using Math.round() will give you a non-uniform distribution!
+   */
+  private getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
