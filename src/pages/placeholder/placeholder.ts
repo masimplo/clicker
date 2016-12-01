ec92a7af884874e0348348b93c00f589794859bf
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2016-12-01 17:20:31 +0200

    Requirement completed

--- src/pages/placeholder/placeholder.ts
+++ src/pages/placeholder/placeholder.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: 'placeholder.html',
+    templateUrl: 'placeholder.html',
+    selector: 'cv-placeholder'
 })
 export class Placeholder {
-
-  public title: string = 'Placeholder';
-
+    public title: string = 'Placeholder';
 }
