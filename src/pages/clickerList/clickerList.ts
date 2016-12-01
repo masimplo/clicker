12211c8c8ceacd51b05a02464cbd3eda55903250
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2016-12-01 21:23:13 +0200

    Modal almost there

--- src/pages/clickerList/clickerList.ts
+++ src/pages/clickerList/clickerList.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, ModalController } from 'ionic-angular';
 import { ClickersService } from '../../services';
 import { Clicker } from '../../models/clicker';
+import { BecomeProModal } from '../become-pro-modal/become-pro.component';
 
 @Component({
   templateUrl: 'clickerList.html',
@@ -12,31 +13,35 @@ export class ClickerList {
   public clickerService: ClickersService;
   public title: string;
   private nav: NavController;
-  private alert: AlertController;
 
-  constructor(nav: NavController, clickerService: ClickersService, alert: AlertController) {
+  constructor(nav: NavController, clickerService: ClickersService, private _modal: ModalController) {
     this.nav = nav;
     this.clickerService = clickerService;
-    this.alert = alert;
     this.title = 'Clickers';
   }
 
+  // public onDeleteClicker(clicker: Clicker) {
+  //   if (clicker.getCount() > 0) {
+  //     const deleteAlert = this.alert.create({
+  //       message: 'Are you sure you want to delete?',
+  //       buttons: [
+  //         {
+  //           text: 'Yes',
+  //           handler: () => {
+  //             this.clickerService.removeClicker(clicker.getId());
+  //           }
+  //         }, 'Cancel'
+  //       ]
+  //     });
+  //     deleteAlert.present();
+  //   } else {
+  //     this.clickerService.removeClicker(clicker.getId());
+  //   }
+  // }
+
   public onDeleteClicker(clicker: Clicker) {
-    if (clicker.getCount() > 0) {
-      const deleteAlert = this.alert.create({
-        message: 'Are you sure you want to delete?',
-        buttons: [
-          {
-            text: 'Yes',
-            handler: () => {
-              this.clickerService.removeClicker(clicker.getId());
-            }
-          }, 'Cancel'
-        ]
-      });
-      deleteAlert.present();
-    } else {
-      this.clickerService.removeClicker(clicker.getId());
-    }
+    const modal = this._modal.create(BecomeProModal);
+    modal.onDidDismiss((d) => d.response && this.clickerService.removeClicker(clicker.getId()));
+    modal.present();
   }
 }
